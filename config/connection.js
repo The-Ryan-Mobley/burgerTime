1785f6fea6d0d535211dabcb64c7e236418fc2ef
Ryan Mobley <ryan.mobley94@gmail.com>
2019-09-25 15:59:15 -0500

    adding jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 const sql = require('mysql');
 require('dotenv').config();
-connection = sql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    port     : 3306,
-    password : process.env.SQL_PW.toString(),
-    database : 'burgers_db',
-});
+if (process.env.JAWSDB_URL) {
+    connection = sql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = sql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        port: 3306,
+        password: process.env.SQL_PW.toString(),
+        database: 'burgers_db',
+    });
+};
 
-connection.connect((err)=> {
+connection.connect((err) => {
     if (err) throw err;
 });
 module.exports = connection;
